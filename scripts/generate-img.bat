e66c7e1fae8c52dd01d6bbbc628c78bab9497fe8
Li Jiaqian <njljq03@gmail.com>
2026-06-01 15:02:09 +0800

    Save final thesis snapshot

--- scripts/generate-img.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-pdflatex --shell-escape generate-img
-DEL "generate-img.pdf"
-DEL "*.aux"
-DEL "*.auxlock"
-DEL "*.dpth"
-DEL "*.log"
-DEL "*.md5"
--- scripts/generate-img.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-pdflatex --shell-escape generate-img
-rm generate-img.pdf
-rm *.aux
-rm *.auxlock
-rm *.dpth
-rm *.log
-rm *.md5
